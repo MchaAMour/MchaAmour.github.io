24d039716a0a5fbceb2580c0410a2fb90430a32c
MchaAMour <mcha.khamis@aalto.fi>
2020-12-12 09:23:45 +0200

    edits

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,8 @@
 $(function(){
   $(".main-nav-bar").load("nav.html");
 });
-
-
-// Mobile Navigation
-// $(window).resize(function(){
-  // if(window.innerWidth < 800) {
-  //   console.log('yes');
-  //   $(".main-nav-bar").empty();
-  //   $(".main-nav-bar").append('<button class="navbar-toggler" type="button" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><div class="menu-btn"><div class="btn-line"></div> <div class="btn-line"></div> <div class="btn-line"></div> </div><span class="menu-text" id="m-text">MENU</span></button><nav class="nav-menu active"><ul><li class="nav-item dropdown"><a class="nav-link" id="navbarDropdownMenuLink" data-toggle="dropdown">SGO HOME</a><ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink"><li><a class="dropdown-item" href="#">OTHERREVIEW</a></li><li><a class="dropdown-item" href="#">CONTACT</a></li></ul></li><li class="nav-item dropdown"><a class="nav-link" id="navbarDropdownMenuLink" data-toggle="dropdown">OSERVATIONS</a><ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink"><li><a class="dropdown-item" href="#">REAL-TIME</a></li><li><a class="dropdown-item" href="#">LATEST DATA</a></li><li><a class="dropdown-item" href="#">DATA ARCHIVE</a></li></ul></li><li class="nav-item dropdown"><a class="nav-link" id="navbarDropdownMenuLink" data-toggle="dropdown">EVENTS</a><ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink"><li><a class="dropdown-item" href="#">SGO BLOG</a></li></ul></li><li class="nav-item dropdown"><a class="nav-link" id="navbarDropdownMenuLink" data-toggle="dropdown">RESEARCH </a><ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink"><li><a class="dropdown-item" href="#">SEISMOLOGY</a></li><li><a class="dropdown-item" href="#">COSMIC RAY GROUP</a></li><li><a class="dropdown-item" href="#">PUBLICATIONS</a></li></ul></li><li class="nav-item dropdown"><a>PROJECTS</a></li><li class="nav-item dropdown"><a class="nav-link" id="navbarDropdownMenuLink" data-toggle="dropdown">STAFF</a><ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink"><li><a class="dropdown-item" href="#">INTERNAL</a></li></ul></li><li class="nav-item dropdown"><a class="nav-link" id="navbarDropdownMenuLink" data-toggle="dropdown">COMMON QUESTION</a><ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink"><li><a class="dropdown-item" href="#">UNIVERSITY OF OULU</a></li></ul></li></ul></nav>');
-
-
-  // }
-// });
+$(function(){
+  $("#sgo-footer").load("footer.html");
+});
 
 
