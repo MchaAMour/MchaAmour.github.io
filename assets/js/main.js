6e385d003fd93e6881733fefcd8b8cb5c10b7c79
nouranelsakiely <nourk632@gmail.com>
2020-12-13 13:12:02 +0200

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,14 @@
 $(function(){
-  $(".main-nav-bar").load("nav.html");
+  $(".main-nav-bar").load("../assets/pages/nav.html");
 });
 $(function(){
-  $("#sgo-footer").load("footer.html");
+  $("#sgo-footer").load("../assets/pages/footer.html");
 });
-
-
+// for closing menu on desktop when click any where
+$('section').on('click', function(){
+  var menu = $('.dropdown-menudesktop');
+  menuBtn.classList.remove("close"); 
+  menu.removeClass("show");
+  document.getElementsByClassName('menu-text')[0].innerHTML = 'MENU';
+  showMenu = false; 
+});
